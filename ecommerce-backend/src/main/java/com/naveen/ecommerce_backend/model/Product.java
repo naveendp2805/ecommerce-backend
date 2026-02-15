a9d000750a7ff28e8f1ad185b55852c20bfd0c41
Naveen <naveendp28122005@gmail.com>
2026-02-15 20:03:13 +0530

    Initial Spring Boot ecommerce backend

---
+++ ecommerce-backend/src/main/java/com/naveen/ecommerce_backend/model/Product.java
@@ -0,0 +1,18 @@
+package com.naveen.ecommerce_backend.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Product {
+
+    @Id
+    private int id;
+
+}
---
+++ ecommerce-backend/src/main/java/com/naveen/ecommerce_backend/repository/ProductRepo.java
@@ -0,0 +1,8 @@
+package com.naveen.ecommerce_backend.repository;
+
+import com.naveen.ecommerce_backend.model.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepo extends JpaRepository<Product,Integer> {
+
+}
